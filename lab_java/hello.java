e304dbe273cf489094275f6b0f8301c33d4f7331
prem <prem.mahita.com@gmail.com>
2022-03-29 02:11:00 +0000

    adding php,java

---
+++ lab_java/hello.java
@@ -0,0 +1,14 @@
+class Hello
+{
+    public static void main (String args[])
+    {
+        System.out.println("Welcome to Java World - Tecadmin.net");
+    }
+class Hello
+{
+    public static void main (String args[])
+    {
+        System.out.println("Welcome to Java World - Tecadmin.net");
+    }
+}
+}
